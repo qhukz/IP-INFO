3f39bdf0d46ca50f8c4430fb695e0a73184113c2
qhukz <90625793+qhukz@users.noreply.github.com>
2022-03-09 13:47:41 +0200

    Update IPTracker.py

--- IPTracker.py
+++ IPTracker.py
@@ -6,36 +6,37 @@ import folium
 def get_info(ip='127.0.0.1'):
     try:
         response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
-        print(response)
+        # print(response)
         
         data = {
             '[IP]': response.get('query'),
-            '[Int Provider]': response.get('isp')
-            '[Country]': response.get('country')
-            '[Region]': response.get('regionName')
-            '[City]': response.get('city')
-            '[Lat]': response.get('lat')
-            '[Lon]': response.get('lon')
-            
+            '[Int prov]': response.get('isp'),
+            '[Org]': response.get('org'),
+            '[Country]': response.get('country'),
+            '[Region Name]': response.get('regionName'),
+            '[City]': response.get('city'),
+            '[ZIP]': response.get('zip'),
+            '[Lat]': response.get('lat'),
+            '[Lon]': response.get('lon'),
         }
         
         for k, v in data.items():
             print(f'{k} : {v}')
-            
+        
         area = folium.Map(location=[response.get('lat'), response.get('lon')])
-        area.save(f'{response.get("query")}_{response.get("city")}.html')
+        area.save(f'{response.get("query")}_{response.get("county")}.html')
         
     except requests.exceptions.ConnectionError:
         print('[!] Please check your connection!')
-
-
+        
+        
 def main():
-    preview_text = Figlet(fonr='slant')
+    preview_text = Figlet(font='slant')
     print(preview_text.renderText('IP INFO'))
-    ip=input('Please enter a target IP: ')
+    ip = input('Please enter a target IP: ')
     
     get_info(ip=ip)
-
-
-if __name__ == '___main__':
+    
+    
+if __name__ == '__main__':
     main()
